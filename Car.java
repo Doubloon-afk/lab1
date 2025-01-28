a63875a2fad922ee81f2fc899373816f76b86f3b
Arvid-P <arvid.petersson1103@gmail.com>
2025-01-28 12:56:27 +0100

    added car

---
+++ Car.java
@@ -0,0 +1,72 @@
+
+package lab1;
+import java.awt.*;
+
+public abstract class Car {
+
+    private final int nrDoors; // Number of doors on the car
+    public double enginePower; // Engine power of the car
+    public double currentSpeed; // The current speed of the car
+    public Color color; // Color of the car
+    private final String modelName; // The car model name
+
+    public Car(int nrDoors, double enginePower, Color color, String modelName) {
+
+       this.nrDoors = nrDoors;
+       this.enginePower = enginePower;
+       this.color = color;
+       this.modelName = modelName;
+       stopEngine();
+
+
+    }
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+        color = clr;
+    }
+
+    public void startEngine(){
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+        currentSpeed = 0;
+    }
+
+    public abstract double speedFactor();
+
+    private void incrementSpeed(double amount){
+        currentSpeed = Math.min(getCurrentSpeed() + speedFactor() * amount,enginePower);
+    }
+
+    private void decrementSpeed(double amount){
+        currentSpeed = Math.max(getCurrentSpeed() - speedFactor() * amount,0);
+    }
+
+
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
+
+}
