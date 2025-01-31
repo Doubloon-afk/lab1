89d20ea968b434be9bb2d9120757bf1c8e2a8235
Arvid-P <arvid.petersson1103@gmail.com>
2025-01-31 15:54:59 +0100

    removed some spaces

--- Volvo240.java
+++ Volvo240.java
@@ -6,11 +6,9 @@ public class Volvo240 extends Car{
 
     private final static double trimFactor = 1.25;
 
-
     public Volvo240(){
         super(4, 100, Color.black, "Volvo240");
     }
-
 
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
